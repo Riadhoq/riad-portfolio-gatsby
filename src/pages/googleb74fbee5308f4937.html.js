511d081f827959e48535baee24e3ec20f5b41818
Riadul Hoque <hoqueriadul@gmail.com>
2019-03-02 15:08:59 -0500

    add google searcher html in public folder and changed gitignore file

--- src/pages/googleb74fbee5308f4937.html.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const GoogleSearch = () => (
-    <html><head></head><body>google-site-verification: googleb74fbee5308f4937.html</body></html>
-)
-
-export default GoogleSearch;
