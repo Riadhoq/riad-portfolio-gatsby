e45347b4ded60bea3ff7cd65d651fa73e481d7dd
Riadul Hoque <hoqueriadul@gmail.com>
2019-03-02 14:29:33 -0500

    add google searcher html file

---
+++ src/pages/googleb74fbee5308f4937.html.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const GoogleSearch = () => (
+    <html><head></head><body>google-site-verification: googleb74fbee5308f4937.html</body></html>
+)
+
+export default GoogleSearch;
