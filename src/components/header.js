c8583d1b9464dafeaf2559b944f17dcb7823273f
Riadul Hoque <hoqueriadul@gmail.com>
2019-03-13 00:02:20 -0400

    completed nav menu

--- src/components/header.js
+++ src/components/header.js
@@ -2,49 +2,63 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import svgElement from '../images/logo.svg';
+import { FaGithubSquare, FaLinkedinIn, FaTwitterSquare, } from 'react-icons/fa';
 
 const Header = () => (
   <header className="header">
-    <Link
-      to="/"
-      className="logo"
-    >
-      <img src={svgElement} alt="Logo" />
-    </Link>
-    <ul>
-      <li>
-        <Link
-          to="/"
-          className="logo"
-        >
-          About
+    <nav className="navbar">
+      <Link
+        to="/"
+        className="logo"
+      >
+        <img src={svgElement} alt="Logo" />
       </Link>
-      </li>
-      <li>
-        <Link
-          to="/"
-          className="logo"
-        >
-          Resume
+      <input type="checkbox" name="menu" id="menu" />
+      <label htmlFor="menu" className="hamburger">
+        <span>Menu</span>
+        <span class="line line-1"></span>
+        <span class="line line-2"></span>
+        <span class="line line-3"></span>
+      </label>
+      <div className="nav-menu-container">
+      <ul className="nav-menu">
+        <li>
+          <Link
+            to="/"
+          >
+            About
       </Link>
-      </li>
-      <li>
-        <Link
-          to="/"
-          className="logo"
-        >
-          Blog
+        </li>
+        <li>
+          <Link
+            to="/"
+          >
+            Resume
       </Link>
-      </li>
-      <li>
-        <Link
-          to="/"
-          className="logo"
-        >
-          Contact
+        </li>
+        <li>
+          <Link
+            to="/"
+          >
+            Blog
       </Link>
-      </li>
-    </ul>
+        </li>
+        <li>
+          <Link
+            to="/"
+          >
+            Contact
+      </Link>
+        </li>
+      </ul>
+      <ul className="menu-social">
+        <li><a href="linkedin"><FaGithubSquare size="3em" /></a></li>
+        <li><a href="linkedin"><FaLinkedinIn size="3em" /></a></li>
+        <li><a href="linkedin"><FaTwitterSquare size="3em" /></a></li>
+      </ul>
+      </div>
+      
+    </nav>
   </header>
 )
 
