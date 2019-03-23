bba7810fcec456b48397400241eea8fe2f96d1f8
Riadul Hoque <hoqueriadul@gmail.com>
2019-03-22 23:32:40 -0400

    Scroller fixer for navmenu

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import svgElement from '../images/logo.svg';
-import { FaGithubSquare, FaLinkedinIn, FaTwitterSquare, } from 'react-icons/fa';
+import { FaGithubSquare, FaLinkedinIn, FaTwitterSquare } from 'react-icons/fa';
 
 const Header = () => (
   <header className="header">
@@ -13,51 +13,51 @@ const Header = () => (
       >
         <img src={svgElement} alt="Logo" />
       </Link>
-      <input type="checkbox" name="menu" id="menu" />
+      <input onChange={e => handleMenuToggle(e)} type="checkbox" name="menu" id="menu" />
       <label htmlFor="menu" className="hamburger">
         <span>Menu</span>
-        <span class="line line-1"></span>
-        <span class="line line-2"></span>
-        <span class="line line-3"></span>
+        <span className="line line-1"></span>
+        <span className="line line-2"></span>
+        <span className="line line-3"></span>
       </label>
       <div className="nav-menu-container">
-      <ul className="nav-menu">
-        <li>
-          <Link
-            to="/"
-          >
-            About
+        <ul className="nav-menu">
+          <li>
+            <Link
+              to="/"
+            >
+              About
       </Link>
-        </li>
-        <li>
-          <Link
-            to="/"
-          >
-            Resume
+          </li>
+          <li>
+            <Link
+              to="/"
+            >
+              Resume
       </Link>
-        </li>
-        <li>
-          <Link
-            to="/"
-          >
-            Blog
+          </li>
+          <li>
+            <Link
+              to="/"
+            >
+              Blog
       </Link>
-        </li>
-        <li>
-          <Link
-            to="/"
-          >
-            Contact
+          </li>
+          <li>
+            <Link
+              to="/"
+            >
+              Contact
       </Link>
-        </li>
-      </ul>
-      <ul className="menu-social">
-        <li><a href="linkedin"><FaGithubSquare size="3em" /></a></li>
-        <li><a href="linkedin"><FaLinkedinIn size="3em" /></a></li>
-        <li><a href="linkedin"><FaTwitterSquare size="3em" /></a></li>
-      </ul>
+          </li>
+        </ul>
+        <ul className="menu-social">
+          <li><a href="linkedin"><FaGithubSquare size="3em" /></a></li>
+          <li><a href="linkedin"><FaLinkedinIn size="3em" /></a></li>
+          <li><a href="linkedin"><FaTwitterSquare size="3em" /></a></li>
+        </ul>
       </div>
-      
+
     </nav>
   </header>
 )
@@ -68,6 +68,10 @@ Header.propTypes = {
 
 Header.defaultProps = {
   siteTitle: ``,
+}
+
+function handleMenuToggle(e) {
+  document.body.style.overflow = e.target.checked ? "hidden" : "unset";
 }
 
 export default Header
